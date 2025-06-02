36693088a1a4eef1bb66753eaf41c6ac064e610c
truonglk1234 <nxtruong2909@gmail.com>
2025-06-02 22:04:24 +0700

    Initial commit

---
+++ src/main/java/com/group1/project/swp_project/SwpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.group1.project.swp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwpProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group1/project/swp_project/SwpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.project.swp_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
